27d3844f0da4ba2a5619a1d1cf0a73f1dd52d87f
jader <yjader@foxmail.com>
2026-06-29 09:57:43 +0000

    fix(package): handle missing generated version

--- telefuser/__init__.py
+++ telefuser/__init__.py
@@ -1,1 +1,6 @@
-from ._version import __version__
+try:
+    from ._version import __version__
+except ModuleNotFoundError as exc:
+    if exc.name != "telefuser._version":
+        raise
+    __version__ = "0.0.0+unknown"
